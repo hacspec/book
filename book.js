47c06be9f98d2fa5278a03efdb2a4f1c869eb1ef
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2024-03-23 12:09:08 +0000

    deploy: fabefe4f93831bd2cc841003d20e143f34eda8f8

--- book.js
+++ book.js
@@ -3,6 +3,59 @@
 // Fix back button cache problem
 window.onunload = function () { };
 
+(() => {
+    console.log(1234);
+    for(let o of document.querySelectorAll('strong')) {
+        console.log(1234);
+        if(o.innerText != 'user-checkable')
+            continue;
+        let node = document.createElement('input');
+        node.type = 'checkbox';
+        node.classList.add('user-checkable-checkbox');
+        o.parentElement.replaceChild(node, o);
+    }
+})();
+
+(() => {
+    let id = 0;
+    for(let fst of document.querySelectorAll('pre + pre')) {
+        let parent = fst.parentElement;
+        let rust = fst.previousElementSibling;
+        let w = s => `<div class=snippet>${s}</div>`;
+        let both = w(rust.outerHTML) + w(fst.outerHTML);
+        parent.removeChild(rust);
+
+        let snippets = document.createElement('div');
+        snippets.classList.add('snippets');
+        snippets.innerHTML = both;
+
+        let buttons = document.createElement('div');
+        id++;
+        buttons.classList.add('buttons');
+        buttons.innerHTML  = `<button>Rust</button><button>F* extraction</button>`;
+        let mk_active = i => {
+            [snippets, buttons].map(el => 
+                [...el.children].map((x, j) => {
+                    x.classList[j==i ? 'remove' : 'add']('fstar-rust-snippet-hidden');
+                    x.classList[j==i ? 'add' : 'remove']('fstar-rust-snippet-active');
+                })
+            );
+        };
+        [...buttons.children].map((child, nth) =>
+            (child.onclick = _ => mk_active(nth))
+        );
+        const RUST = 0;
+        mk_active(RUST);
+        
+        let wrapper = document.createElement('div');
+        wrapper.classList.add('rust-fstar-wrapper');
+        wrapper.appendChild(buttons);
+        wrapper.appendChild(snippets);
+
+        parent.replaceChild(wrapper, fst);
+    }
+})();
+
 // Global variable, shared between modules
 function playground_text(playground, hidden = true) {
     let code_block = playground.querySelector("code");
